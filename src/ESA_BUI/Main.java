a9a017fc52529f9b956aff4e6ed7e7e440cc999a
RaphaelFFFF <raphael48@hotmail.de>
2025-05-19 17:36:15 +0200

    Update Main.java

--- src/ESA_BUI/Main.java
+++ src/ESA_BUI/Main.java
@@ -2,6 +2,6 @@ package ESA_BUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo, World!");
+        System.out.println("Hallo World!");
     }
 }
